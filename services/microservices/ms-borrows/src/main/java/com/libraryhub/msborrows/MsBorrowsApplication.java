7835cbce904c9d29f1085e238747bf78ed0e2218
yassineB999 <belouchi2001@gmail.com>
2024-11-30 17:26:56 +0100

    Initialize ms-borrows microservice project
     -Add application structure, configuration, and build files

---
+++ services/microservices/ms-borrows/src/main/java/com/libraryhub/msborrows/MsBorrowsApplication.java
@@ -0,0 +1,13 @@
+package com.libraryhub.msborrows;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsBorrowsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsBorrowsApplication.class, args);
+    }
+
+}
