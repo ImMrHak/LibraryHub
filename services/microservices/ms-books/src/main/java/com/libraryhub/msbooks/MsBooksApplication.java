60d7abd6ab40079848684c79e13740e62402f255
ImMrHak <hakkoumohamed23@gmail.com>
2024-11-30 17:31:13 +0100

    feat: initialize ms-reservations microservice and enable discovery client
    - Initialized the ms-reservations microservice project structure.
    - Added `@EnableDiscoveryClient` annotation across all microservices for service discovery.
    - Updated relevant configuration files.

--- services/microservices/ms-books/src/main/java/com/libraryhub/msbooks/MsBooksApplication.java
+++ services/microservices/ms-books/src/main/java/com/libraryhub/msbooks/MsBooksApplication.java
@@ -2,8 +2,10 @@ package com.libraryhub.msbooks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MsBooksApplication {
 
     public static void main(String[] args) {
--- services/microservices/ms-reservations/src/main/java/com/libraryhub/msreservations/MsReservationsApplication.java
+++ services/microservices/ms-reservations/src/main/java/com/libraryhub/msreservations/MsReservationsApplication.java
@@ -1,4 +1,4 @@
-package com.libraryhub.msborrows;
+package com.libraryhub.msreservations;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class MsBorrowsApplication {
+public class MsReservationsApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MsBorrowsApplication.class, args);
+        SpringApplication.run(MsReservationsApplication.class, args);
     }
 
 }
